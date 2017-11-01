20c5b58444cd74f394e285a728ed84318737e4f5
Mithilan Sivanesan <mithilan91@gmail.com>
2017-11-01 04:21:54 -0400

    implemented getSha1Digest function

---
+++ getSha1Digest.py
@@ -0,0 +1,9 @@
+import hashlib
+## The following function returns the sha1 digest from a file containing text
+## To use, call the function as follows print getSha1Digest("path/to/file")
+def getSha1Digest(path):
+    f = open(path)
+    path = f.read()
+    m = hashlib.sha1(path)
+
+    return m.hexdigest()
